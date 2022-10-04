301223ccf939065a9279257bc87b921981345cc0
protsenkovlad25 <vladislav.procenko.2002@mail.ru>
2022-10-04 23:20:13 +0700

    Added Catalog class

---
+++ Filmolib/Models/Entities/Catalog.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Filmolib.Models.Entities
+{
+    class Catalog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Ranked { get; set; }
+
+        public int? UserId { get; set; }
+        public User User { get; set; }
+
+        public ICollection<Film> Films { get; set; }
+
+        public Catalog()
+        {
+            Films = new List<Film>();
+        }
+    }
+}
