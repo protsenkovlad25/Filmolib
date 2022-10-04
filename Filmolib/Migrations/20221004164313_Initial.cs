0282cbbfebe84ae37abb111dd3a3b1125d6be223
protsenkovlad25 <vladislav.procenko.2002@mail.ru>
2022-10-05 00:37:50 +0700

    Added Migrations and changes

---
+++ Filmolib/Migrations/20221004164313_Initial.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Filmolib.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Films",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Rating = table.Column<float>(type: "real", nullable: false),
+                    Director = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PosterSource = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Films", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Patronymic = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AvatarSource = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Catalogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ranked = table.Column<bool>(type: "bit", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Catalogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Catalogs_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CatalogFilm",
+                columns: table => new
+                {
+                    CatalogsId = table.Column<int>(type: "int", nullable: false),
+                    FilmsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogFilm", x => new { x.CatalogsId, x.FilmsId });
+                    table.ForeignKey(
+                        name: "FK_CatalogFilm_Catalogs_CatalogsId",
+                        column: x => x.CatalogsId,
+                        principalTable: "Catalogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CatalogFilm_Films_FilmsId",
+                        column: x => x.FilmsId,
+                        principalTable: "Films",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatalogFilm_FilmsId",
+                table: "CatalogFilm",
+                column: "FilmsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Catalogs_UserId",
+                table: "Catalogs",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CatalogFilm");
+
+            migrationBuilder.DropTable(
+                name: "Catalogs");
+
+            migrationBuilder.DropTable(
+                name: "Films");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- Filmolib/ViewModels/RegistrationViewModel.cs
+++ Filmolib/ViewModels/RegistrationViewModel.cs
@@ -1,13 +1,52 @@
-﻿using System;
+﻿using Filmolib.Models.Entities;
+using Filmolib.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Filmolib.ViewModels
 {
     class RegistrationViewModel : Base.BaseViewModel
     {
+        private readonly FilmolibContext _db;
 
+        public string? NewUsername { get; set; }
+        public string? NewPassword { get; set; }
+        public string? NewEmail { get; set; }
+
+        public RegistrationViewModel()
+        {
+            _db = new FilmolibContext();
+        }
+
+        public ICommand AddUser
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    //User newUser = new User()
+                    //{
+                    //    Username = NewUsername,
+                    //    Password = NewPassword,
+                    //    Email = NewEmail,
+                    //    AvatarSource = "",
+                    //    Name = "",
+                    //    Surname = "",
+                    //    Patronymic = "",
+                    //    PhoneNumber = ""
+                    //};
+
+                    //if (_db.Users.Add(newUser).State != EntityState.Added)
+                    //    throw new DbUpdateException($"{newUser.Username} could not save to database");
+                    //if (_db.SaveChanges() < 1)
+                    //    throw new DbUpdateException($"{newUser.Username} could not save to database");
+                });
+            }
+        }
     }
 }
