8c521205ec390244c8822020e581ec170f5b30bd
protsenkovlad25 <vladislav.procenko.2002@mail.ru>
2022-10-04 23:17:44 +0700

    Added Film class

---
+++ Filmolib/Models/Entities/Film.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Filmolib.Models.Entities
+{
+    class Film
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Year { get; set; }
+        public float Rating { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public string PosterSource { get; set; }
+
+        public ICollection<Catalog> Catalogs { get; set; }
+
+        public Film()
+        {
+            Catalogs = new List<Catalog>();
+        }
+    }
+}
