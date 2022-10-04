f191ce9958957634c7dc6ed4636ae3663b6024a9
protsenkovlad25 <vladislav.procenko.2002@mail.ru>
2022-10-04 23:38:16 +0700

    Added database connection

---
+++ Filmolib/Models/FilmolibContext.cs
@@ -0,0 +1,27 @@
+﻿using Filmolib.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filmolib.Models
+{
+    class FilmolibContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Film> Films { get; set; }
+        public DbSet<Catalog> Catalogs { get; set; }
+
+        public FilmolibContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(local);Database=FilmolibDB;Trusted_Connection=True;");
+        }
+    }
+}
