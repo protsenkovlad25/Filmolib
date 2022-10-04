a95836e70feada7b595fd9da33e9960b69a1c6af
protsenkovlad25 <vladislav.procenko.2002@mail.ru>
2022-10-04 23:02:08 +0700

    Added MainViewModel class and changed MainWindow

---
+++ Filmolib/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Filmolib.ViewModels
+{
+    class MainViewModel : Base.BaseViewModel
+    {
+        public UserControl ContentPage { get; set; }
+
+        public ICommand RegistrationPage
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    //ContentPage = new RegistrationPage();
+                });
+            }
+        }
+    }
+}
