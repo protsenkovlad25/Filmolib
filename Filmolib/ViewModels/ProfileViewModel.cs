a4f751246404cce26a841d3881a8fd68ec069433
protsenkovlad25 <vladislav.procenko.2002@mail.ru>
2022-10-08 22:01:55 +0700

    Updated Profile page and added ProfileViewModel

---
+++ Filmolib/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Filmolib.Models;
+using System.Windows.Input;
+
+namespace Filmolib.ViewModels
+{
+    class ProfileViewModel : Base.BaseViewModel
+    {
+        private FilmolibContext _db;
+
+        public string UserUsername { get; set; }
+        public string UserPassword { get; set; }
+        public string UserEmail { get; set; }
+
+        public ProfileViewModel()
+        {
+            _db = new FilmolibContext();
+        }
+
+        private void Initialize()
+        {
+            
+        }
+
+        public ICommand Command
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+
+                });
+            }
+        }
+    }
+}
